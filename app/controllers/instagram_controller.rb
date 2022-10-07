7bcbd23b06ca173c456ef709b3b34dc8cf5dc420
Buvanesh-P <bwaran111@gmail.com>
2022-10-07 15:12:12 +0530

    Skip auth for insta controller fix

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -2,7 +2,7 @@ class InstagramController < ApplicationController
     skip_before_action :authenticate_user!
     def auth 
         auth_code = params[:code]
-        rendor json: {"auth_code": auth_code}
+        render json: {"auth_code": auth_code}
     end
     def fbauth
         render json:{"access_token": params[:access_token] }
