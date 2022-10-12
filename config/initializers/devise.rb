5dfe9ec578843250d6ea2ccd0ad8a1166d974786
Buvanesh-P <bwaran111@gmail.com>
2022-10-12 12:10:38 +0530

    dummy cred test

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,8 +25,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECERET']
+  config.omniauth :google_oauth2, '827539953091-4qkvql9iqps62sciavfi53eece6jaqvd.apps.googleusercontent.com', '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'
+  config.omniauth :facebook, '632092495018336', '8eadc66a37da7e324dec45787ae74460'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
